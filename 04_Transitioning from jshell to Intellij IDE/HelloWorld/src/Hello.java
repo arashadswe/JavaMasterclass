f960408a2b8b21f817ffee7ae2b4287c48c58a7f
Ahmed Rashad <ahm.rashad.202@gmail.com>
2025-09-08 04:16:37 +0300

    Logical AND operator &&

--- 04_Transitioning from jshell to Intellij IDE/HelloWorld/src/Hello.java
+++ 04_Transitioning from jshell to Intellij IDE/HelloWorld/src/Hello.java
@@ -1,10 +1,26 @@
 public class Hello {
+
     public static void main (String[] args) {
+
         System.out.println("Hello, Ahmed");
 
         boolean isAlien = false;
         if (isAlien == false) {
             System.out.println("It is not an Alien!");
+            System.out.println("And I am scared of aliens");
         }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+
     }
+
 }
