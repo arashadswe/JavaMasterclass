e75c4d61fe965f1908ea4130b255fa31e9e92a72
Ahmed Rashad <ahm.rashad.202@gmail.com>
2025-09-08 03:55:37 +0300

    if-then statement

--- 04_Transitioning from jshell to Intellij IDE/HelloWorld/src/Hello.java
+++ 04_Transitioning from jshell to Intellij IDE/HelloWorld/src/Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
     public static void main (String[] args) {
         System.out.println("Hello, Ahmed");
-        System.out.println("Hello World");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien!");
+        }
     }
 }
