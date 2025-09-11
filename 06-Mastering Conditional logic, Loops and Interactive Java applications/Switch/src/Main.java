5a7c4f8225fbe4933552846e6098ce274210df04
Ahmed Rashad <ahm.rashad.swe@gmail.com>
2025-09-11 13:09:11 +0300

    Advanced switch statement (switch expression)

--- 06-Mastering Conditional logic, Loops and Interactive Java applications/Switch/src/Main.java
+++ 06-Mastering Conditional logic, Loops and Interactive Java applications/Switch/src/Main.java
@@ -11,20 +11,15 @@ public class Main {
 //            System.out.println("Was not 1 or 2");
 //        }
 
-        int switchValue = 0;
+        int switchValue = 1;
         switch (switchValue) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-                break;
-            case 3: case 4: case 5:
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Was a 3, a 4, or a 5");
                 System.out.println("Actually it was a " + switchValue);
-                break;
-            default:
-                System.out.println("Was not 1, 2, 3, 4, or 5");
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
     }
 }
