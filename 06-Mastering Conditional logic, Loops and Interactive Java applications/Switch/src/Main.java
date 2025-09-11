4fbc758ff1dc7213fd532c321e1da290d2bb7c3f
Ahmed Rashad <ahm.rashad.swe@gmail.com>
2025-09-11 13:19:24 +0300

    getQuarter with old switch

--- 06-Mastering Conditional logic, Loops and Interactive Java applications/Switch/src/Main.java
+++ 06-Mastering Conditional logic, Loops and Interactive Java applications/Switch/src/Main.java
@@ -21,5 +21,34 @@ public class Main {
             }
             default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
+
+        String month = "APRIL";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+    }
+
+    public static String getQuarter(String month) {
+
+        switch (month) {
+            case "JANUARY":
+            case "FEBRUARY":
+            case "MARCH":
+                return "1st";
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return "2nd";
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return "3rd";
+            case "OCTOBER":
+            case "NOVEMBER":
+            case "DECEMBER":
+                return "4th";
+        }
+
+        return "bad";
     }
 }
+
+
