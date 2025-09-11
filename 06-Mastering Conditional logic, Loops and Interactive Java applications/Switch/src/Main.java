ae3b1fe49baf5cd6c1790a93836bbb69d5ff7884
Ahmed Rashad <ahm.rashad.swe@gmail.com>
2025-09-11 13:21:09 +0300

    getQuarter with switch expression

--- 06-Mastering Conditional logic, Loops and Interactive Java applications/Switch/src/Main.java
+++ 06-Mastering Conditional logic, Loops and Interactive Java applications/Switch/src/Main.java
@@ -28,26 +28,14 @@ public class Main {
 
     public static String getQuarter(String month) {
 
-        switch (month) {
-            case "JANUARY":
-            case "FEBRUARY":
-            case "MARCH":
-                return "1st";
-            case "APRIL":
-            case "MAY":
-            case "JUNE":
-                return "2nd";
-            case "JULY":
-            case "AUGUST":
-            case "SEPTEMBER":
-                return "3rd";
-            case "OCTOBER":
-            case "NOVEMBER":
-            case "DECEMBER":
-                return "4th";
-        }
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> "bad";
+        };
 
-        return "bad";
     }
 }
 
