d8fa2e9d49485f9a43b0bdbbaa4b5edb6c3b2a50
Ahmed Rashad <ahm.rashad.swe@gmail.com>
2025-09-12 05:01:54 +0300

    Basic for loop

---
+++ 06-Mastering Conditional logic, Loops and Interactive Java applications/TheForLoop/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println(counter);
+        }
+    }
+}
