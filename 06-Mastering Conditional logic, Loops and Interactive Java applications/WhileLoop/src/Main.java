fdf091c9f9e8f76ffe951d4230fbf86b6445f875
Ahmed Rashad <ahm.rashad.swe@gmail.com>
2025-09-14 10:54:16 +0300

    Continue statement

--- 06-Mastering Conditional logic, Loops and Interactive Java applications/WhileLoop/src/Main.java
+++ 06-Mastering Conditional logic, Loops and Interactive Java applications/WhileLoop/src/Main.java
@@ -2,20 +2,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+//
+//        int j = 1;
+//        boolean isReady = false;
+//        do {
+//
+//            if ( j > 5) {
+//                break;
+//            }
+//
+//            System.out.println(j);
+//            j++;
+//        } while (isReady);
 
-        int j = 1;
-        boolean isReady = false;
-        do {
-
-            if ( j > 5) {
-                break;
+        int number = 0;
+        while (number < 50) {
+            number += 5;
+            if (number % 25 == 0) {
+                continue;
             }
-
-            System.out.println(j);
-            j++;
-        } while (isReady);
+            System.out.print(number + "_");
+        }
     }
 }
