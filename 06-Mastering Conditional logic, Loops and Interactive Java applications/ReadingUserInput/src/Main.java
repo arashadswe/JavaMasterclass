0791f28dd1a9b8ff3d7bffbeae4cc2a81c308454
Ahmed Rashad <ahm.rashad.swe@gmail.com>
2025-09-15 13:14:27 +0300

    getting user input using System.console()

--- 06-Mastering Conditional logic, Loops and Interactive Java applications/ReadingUserInput/src/Main.java
+++ 06-Mastering Conditional logic, Loops and Interactive Java applications/ReadingUserInput/src/Main.java
@@ -7,5 +7,22 @@ public class Main {
         int dateOfBirth = Integer.parseInt(userDateOfBirth);
 
         System.out.println("Age = " + (currentYear - dateOfBirth));
+        System.out.println(getInputFromConsole(currentYear));
+    }
+
+    public static String getInputFromConsole(int currentYear) {
+
+        String name = System.console().readLine("Hi, Whats your name? ");
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+
+        String dateOfBirth = System.console().readLine("What year where you born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " years old";
+    }
+
+    public static String getInputScanner(int currentYear) {
+
+        return "";
     }
 }
