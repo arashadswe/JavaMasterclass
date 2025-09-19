c805e9e45bdb9164d3a750bca4ae0c8969ceac26
Ahmed Rashad <ahm.rashad.swe@gmail.com>
2025-09-19 04:04:45 +0300

    POJO implementation

--- 07- Mastering Java OOP _ Classes and Inheritance/RecordProject/src/Main.java
+++ 07- Mastering Java OOP _ Classes and Inheritance/RecordProject/src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
-    
+
     public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            Student s = new Student("S29300" + i,
+                    switch(i) {
+                        case 1 -> "Mary";
+                        case 2 -> "Carol";
+                        case 3 -> "Tim";
+                        case 4 -> "Hary";
+                        case 5 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    "05/11/1985",
+                    "Java Masterclass");
+
+            System.out.println(s);
+        }
+
 
     }
 }
